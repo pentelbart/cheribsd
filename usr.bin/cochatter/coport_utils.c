1cfc3cdd51c3e910a3f777a79c664711c8b4c499
pentelbart <46610525+pentelbart@users.noreply.github.com>
2020-02-27 00:17:58 +0000

    Fixed some use-before-init

--- usr.bin/cochatter/coport_utils.c
+++ usr.bin/cochatter/coport_utils.c
@@ -12,8 +12,6 @@
 
 int init_port(coport_type_t type, coport_t * p)
 {
-	int error;
-
 	/*
 	 * TODO-PBB: We will replace this mmap call so that its job is performed by
 	 * a worker thread ahead of time, avoiding the context switch from the 
@@ -21,10 +19,12 @@ int init_port(coport_type_t type, coport_t * p)
 	 */
 	p->buffer=mmap(0,COPORT_BUF_LEN,COPORT_MMAP_PROT,COPORT_MMAP_FLAGS,-1,0);
 	p->length=COPORT_BUF_LEN;
+	memset(p->buffer,0,p->length);
 	p->status=COPORT_OPEN;
 	p->start=0;
 	p->end=0;
 	p->status=COPORT_OPEN;
+	p->type=type;
 
 	return 0;
 }
